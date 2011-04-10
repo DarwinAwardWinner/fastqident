9371a541b8cc9dae6e58e10d38be1c4e8a643b67
Ryan C. Thompson <rct@thompsonclan.org>
2011-04-10 15:23:30 -0700

    Convert to separate fastqident module

---
+++ README.md
@@ -0,0 +1,8 @@
+# fastqident: Guess the quality encoding system used by FASTQ sequence files.
+
+This script was previously part of a larger package, and I have
+decided to split it out into a python package of its own. As a result,
+the interface is changing. As soon as it stabilizes, I will document
+it here.
+
+
---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+def read_file(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "fastqident",
+    version = "0.1dev",
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = "Ryan C. Thompson",
+    author_email = "rct@thompsonclan.org",
+    description = "Guess the quality encoding system used by FASTQ sequence files.",
+    license = "BSD",
+    keywords = ("biopython", "bioinformatics", "fastq", "sequencing")
+    #url = "http://example.com/HelloWorld/",   # project home page, if any
+    long_description=read_file('README.md'),
+)
