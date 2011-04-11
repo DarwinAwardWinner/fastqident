427a9c22789b893cc91da501a67d56dd72211f9e
Ryan C. Thompson <rct@thompsonclan.org>
2011-04-11 02:27:04 -0700

    setup.py syntax fixes

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ setup(
     packages = find_packages(),
     entry_points = {
         'console_scripts': [
-            'fastqident = fastqident.main.plac_call_main',
+            'fastqident = fastqident.main:plac_call_main',
         ],
-    }
+    },
 
     # metadata for upload to PyPI
     author = "Ryan C. Thompson",
     author_email = "rct@thompsonclan.org",
     description = "Guess the quality encoding system used by FASTQ sequence files.",
     license = "BSD",
-    keywords = ("biopython", "bioinformatics", "fastq", "sequencing")
+    keywords = ("biopython", "bioinformatics", "fastq", "sequencing"),
     #url = "http://example.com/HelloWorld/",   # project home page, if any
     long_description=read_file('README.md'),
 )
